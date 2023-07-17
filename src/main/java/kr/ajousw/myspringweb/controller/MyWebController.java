50907e63cdd97bcd68650be75f8fef93efa49dd6
lucple <dlwnstj99@naver.com>
2023-07-17 16:13:24 +0900

    [Add] post_likes

--- src/main/java/kr/ajousw/myspringweb/controller/MyWebController.java
+++ src/main/java/kr/ajousw/myspringweb/controller/MyWebController.java
@@ -1,15 +1,24 @@
 package kr.ajousw.myspringweb.controller;
 
 import java.io.IOException;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.transaction.Transactional;
+import kr.ajousw.myspringweb.dto.FavoriteMusicRequestDto;
 import kr.ajousw.myspringweb.dto.MusicList;
+import kr.ajousw.myspringweb.entity.FavoriteMusic;
+import kr.ajousw.myspringweb.repository.FavoriteRepository;
 
 @RestController
 public class MyWebController {
@@ -50,5 +59,30 @@ public class MyWebController {
             System.out.println(e.toString());
         }
         return null;
+    }
+
+    @Autowired
+    FavoriteRepository albumsRepo;
+
+    @GetMapping(value = "/likes")
+    public List<FavoriteMusic> getLikes() {
+        try {
+            return albumsRepo.findAll();
+        } catch (Exception e) {
+            System.out.println(e.toString());
+            return null;
+        }
+
+    }
+
+    @PostMapping(value = "/likes")
+    @Transactional
+    public int postLikes(@RequestBody FavoriteMusicRequestDto favorite) {
+        FavoriteMusic music = albumsRepo.save(favorite.toEntity());
+        if (music != null) {
+            return 1;
+        } else {
+            return 0;
+        }
     }
 }
