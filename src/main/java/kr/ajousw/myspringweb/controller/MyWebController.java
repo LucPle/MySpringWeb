79c973547846166e26323f5c2a9781a589140c7b
lucple <dlwnstj99@naver.com>
2023-07-20 16:56:31 +0900

    [Modify] Finish

--- src/main/java/kr/ajousw/myspringweb/controller/MyWebController.java
+++ src/main/java/kr/ajousw/myspringweb/controller/MyWebController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import kr.ajousw.myspringweb.entity.FavoriteMusic;
 import kr.ajousw.myspringweb.repository.FavoriteRepository;
 import kr.ajousw.myspringweb.service.MusicService;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 
 public class MyWebController {
--- src/main/java/kr/ajousw/myspringweb/entity/FavoriteMusic.java
+++ src/main/java/kr/ajousw/myspringweb/entity/FavoriteMusic.java
@@ -2,6 +2,8 @@ package kr.ajousw.myspringweb.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
